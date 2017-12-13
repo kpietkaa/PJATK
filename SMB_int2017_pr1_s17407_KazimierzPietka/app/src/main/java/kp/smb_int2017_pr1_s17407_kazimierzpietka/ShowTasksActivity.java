602d6be60b9e7c0a6172636227bc15dfd77d3b2d
Kazik Pietka <kazimierz.pietka@infopark.de>
2017-12-13 22:44:48 +0100

    :fire: Old code

--- SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/ShowTasksActivity.java
+++ SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/ShowTasksActivity.java
@@ -2,8 +2,6 @@ package kp.smb_int2017_pr1_s17407_kazimierzpietka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,27 +16,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShowTasksActivity extends AppCompatActivity {
-//    ListView listView;
-//    private SQLiteDatabaseHandler db;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_show_tasks);
-//
-//        db = new SQLiteDatabaseHandler(this);
-//        List<Task> tasks;
-//        tasks = db.allTasks();
-//
-//        final CustomListAdapter tasksList = new CustomListAdapter(this, tasks);
-//        listView = (ListView) findViewById(R.id.listviewID);
-//        listView.setAdapter(tasksList);
-//    }
     private static final String JSON_URL = "https://task-backend-sinatra.herokuapp.com/tasks";
     ListView listView;
     List<Task> tasks;
@@ -55,7 +36,6 @@ public class ShowTasksActivity extends AppCompatActivity {
     }
 
     private void loadTasks(){
-        //creating a string request to send request to the url
         StringRequest stringRequest = new StringRequest(Request.Method.GET, JSON_URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
