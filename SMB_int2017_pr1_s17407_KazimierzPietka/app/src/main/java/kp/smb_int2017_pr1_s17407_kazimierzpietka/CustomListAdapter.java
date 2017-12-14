8d5369f0ee45132ac0fde4fa077ec2159c972e16
Kazik Pietka <kazimierz.pietka@infopark.de>
2017-12-14 22:53:26 +0100

    :building_construction: Delete task on backend

--- SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/CustomListAdapter.java
+++ SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/CustomListAdapter.java
@@ -1,6 +1,8 @@
 package kp.smb_int2017_pr1_s17407_kazimierzpietka;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +11,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomListAdapter extends ArrayAdapter<Task> {
-    private List<Task> taskList;
+    private static final String URL = "https://task-backend-sinatra.herokuapp.com/tasks";
+    private final List<Task> taskList;
     private Context mCtx;
     Button edit = null;
     Button destroy = null;
@@ -46,6 +58,25 @@ public class CustomListAdapter extends ArrayAdapter<Task> {
             @Override
             public void onClick(View v) {
                 Toast.makeText(mCtx, "Usuwanie: " + taskList.get(position).getName(), Toast.LENGTH_SHORT).show();
+                String deleteUrl = URL + "/" + taskList.get(position).getId();
+                StringRequest deleteRequest = new StringRequest(Request.Method.DELETE, deleteUrl,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("Response", response);
+                            Toast.makeText(mCtx, response, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(mCtx, "Odśwież widok :)", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("Error.Response", error.toString());
+                        }
+                    }
+                );
+                RequestQueue requestQueue = Volley.newRequestQueue(mCtx);
+                requestQueue.add(deleteRequest);
             }
         });
 
