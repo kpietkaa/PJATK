bdea3e7ef3162d5e7e62de13557df148a567f648
Kazik Pietka <kazimierz.pietka@infopark.de>
2017-12-14 21:51:33 +0100

    :building_construction: Add product to backend

--- SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/AddProductActivity.java
+++ SMB_int2017_pr1_s17407_KazimierzPietka/app/src/main/java/kp/smb_int2017_pr1_s17407_kazimierzpietka/AddProductActivity.java
@@ -1,16 +1,27 @@
 package kp.smb_int2017_pr1_s17407_kazimierzpietka;
 
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class AddProductActivity extends AppCompatActivity {
     private SQLiteDatabaseHandler db;
+    private static final String JSON_URL = "https://task-backend-sinatra.herokuapp.com/tasks";
+
     EditText name, price, quantity;
 
     @Override
@@ -37,27 +48,37 @@ public class AddProductActivity extends AppCompatActivity {
     }
 
     public void addProductToDb(View view) {
-        // create our sqlite helper class
-        db = new SQLiteDatabaseHandler(this);
-
         name =      (EditText) findViewById(R.id.input_name);
         price =     (EditText) findViewById(R.id.input_price);
         quantity =  (EditText) findViewById(R.id.quantity_input);
 
-        Random r = new Random();
-        int id = r.nextInt((int)Math.pow(10,9));
-
-        Task task = new Task();
-        task.setId(id);
-        task.setName(name.getText().toString());
-        task.setPrice(Integer.parseInt(price.getText().toString()));
-        task.setQuantity(Integer.parseInt(quantity.getText().toString()));
-
-        db.addTask(task);
-
-        Toast.makeText(getBaseContext(), R.string.add_task_success, Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, ShowTasksActivity.class);
-        startActivity(intent);
+        StringRequest postRequest = new StringRequest(Request.Method.POST, JSON_URL,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d("Response", response);
+                    Toast.makeText(getBaseContext(), "Dodawanie produktu...", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getBaseContext(), ShowTasksActivity.class);
+                    startActivity(intent);
+                }
+        },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Error.Response", error.toString());
+                }
+            }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name.getText().toString());
+                params.put("price", price.getText().toString());
+                params.put("quantity", quantity.getText().toString());
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(postRequest);
     }
 }
