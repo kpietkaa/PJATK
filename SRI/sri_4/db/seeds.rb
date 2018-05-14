398d7583eb4a41768d8cf78b15ba131255cb1c54
Kazik Pietka <kazimierz.pietka@infopark.de>
2018-05-14 23:37:24 +0200

    :wrench: Boilerplate data

--- SRI/sri_4/db/seeds.rb
+++ SRI/sri_4/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Create user'
+user = User.create!(name: 'Test', surname: 'Test')
+puts 'Create post'
+post = Post.create!(title: 'Test title', body: 'Body body body body', user_id: user.id)
+puts 'Create comment'
+Comment.create!(body: 'Comment comment comment comment', post_id: post.id, user_id: user.id)
